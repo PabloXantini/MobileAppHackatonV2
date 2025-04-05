2dcf0eeb973338892454c02a2ee6735a5f9f7832
PabloXantini <santinypabloe130@gmail.com>
2025-04-05 13:21:55 -0600

    test: checkpoint for test for Node.js

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+//Aqui voy a usar a SW para guardar temporalmente las cosas
+const info1 = document.querySelector('#formInicio');
+const info2 = document.querySelector('#fomrFinal');
+
+if ("sw" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
